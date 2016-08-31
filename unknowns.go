b97161478dac7488fdcaec5d8a091091d7704b64
ross-spencer <allalongthewatchtower2001@gmail.com>
2016-08-31 17:16:53 +1200

    [ignore] Cleaning up while the code is stalling.

--- unknowns.go
+++ unknowns.go
@@ -8,3 +8,8 @@ package main
 
 //Unknown: see https://msdn.microsoft.com/en-us/library/windows/desktop/cc144090(v=vs.85).aspx for clues...
 var found_shortcut_PIDL = [17]byte{0x50, 0xe0, 0x4f, 0xd0, 0x20, 0xea, 0x3a, 0x69, 0x10, 0xa2, 0xd8, 0x08, 0x00, 0x2b, 0x30, 0x30, 0x9d}
+
+//shell bag {search term in google also}
+//https://files.sans.org/summit/Digital_Forensics_and_Incident_Response_Summit_2015/PDFs/PlumbingtheDepthsShellBagsEricZimmerman.pdf
+//http://www.williballenthin.com/forensics/shellbags/
+
